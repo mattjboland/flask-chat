0d4e11b02e6a93fc624509a99b2299b97dcd56b0
Matt Boland <mattjamesboland@gmail.com>
2020-05-27 10:30:38 +0000

    add the ability to write and to read from a listand display the messages to a user

--- run.py
+++ run.py
@@ -7,24 +7,30 @@ messages = []
 
 
 def add_messages(username, message):
+    """Add messages to the `messages` list"""
     messages.append("{}: {}".format(username, message))
+
+
+def get_all_messages():
+    """Get all of the messages and separate them with a `br`"""
+    return "<br>".join(messages)
 
 
 @app.route("/")
 def index():
-    """ Main page with instructions """
+    """Main page with instructions"""
     return "To send a message use: /USERNAME/MESSAGE"
 
 
 @app.route("/<username>")
 def user(username):
-    """ Display chat messages """
-    return "Welcome {0} - {1}".format(username)
+    """Display chat messages"""
+    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
 
 
 @app.route("/<username>/<message>")
 def send_message(username, message):
-    """ Create a new message and redirect back to the chat page """
+    """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect("/" + username)
 
